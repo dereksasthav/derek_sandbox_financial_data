38348b01ebbf20744e9a69bf81584620d3f59e41
dereksasthav <84297324+dereksasthav@users.noreply.github.com>
2021-05-19 17:59:31 -0500

    Update ar_aging_validation.sql

--- ar_aging_validation.sql
+++ ar_aging_validation.sql
@@ -13,7 +13,7 @@ with gcprod as (
 
     SELECT 
         COMPANY
-        , COUNT("DOCUMENT NO.") as numDocuments_GCPROD
+        , COUNT(*) as numRecords_GCPROD
         , SUM("AR MONTHEND BALANCE") as balance_gcprod
     FROM DATAWAREHOUSE.GC_PROD_WH.RAW_ACCOUNTS_RECEIVABLE_MONTHLY
     GROUP BY COMPANY
@@ -22,7 +22,7 @@ with gcprod as (
 
     SELECT  
         COMPANY
-        , COUNT("DOCUMENT NO.") as numDocuments_zap
+        , COUNT(*) as numRecords_zap
         , SUM("AR MONTHEND BALANCE") as balance_zap
     FROM DATAWAREHOUSE.ZAP_BIZ_CENTRAL.STAGE_ACCOUNTS_RECEIVABLE_MONTHLY
     GROUP BY COMPANY
@@ -32,14 +32,60 @@ with gcprod as (
 select 
     gcprod.company as company_gcprod
     , zap.company as company_zap
-    , numDocuments_GCPROD
-    , numDocuments_zap
-    , numDocuments_zap - numDocuments_GCPROD as numDocuments_ZapMinusGCPROD
+    , numRecords_GCPROD
+    , numRecords_zap
+    , numRecords_zap - numRecords_GCPROD as numRecords_ZapMinusGCPROD
     , balance_gcprod
     , balance_zap
     , balance_zap - balance_gcprod as balance_ZapMinusGCPROD
 FROM 
     gcprod FULL OUTER JOIN  zap on gcprod.company = zap.company
+
+/** check document counts **/
+with gcprod as (
+
+    SELECT 
+        COMPANY
+        , "DOCUMENT NO." as document_no
+        , COUNT(*) as numRecords_GCPROD
+        , SUM("AR MONTHEND BALANCE") as balance_gcprod
+    FROM DATAWAREHOUSE.GC_PROD_WH.RAW_ACCOUNTS_RECEIVABLE_MONTHLY
+    WHERE company = '20.02-SESC'
+    GROUP BY COMPANY
+        , "DOCUMENT NO."
+
+), zap as (
+
+    SELECT  
+        COMPANY
+        , "DOCUMENT NO." as document_no
+        , COUNT(*) as numRecords_zap
+        , SUM("AR MONTHEND BALANCE") as balance_zap
+    FROM DATAWAREHOUSE.ZAP_BIZ_CENTRAL.STAGE_ACCOUNTS_RECEIVABLE_MONTHLY
+    WHERE company = '20.02-SESC'
+    GROUP BY COMPANY
+        , "DOCUMENT NO."
+
+)
+
+select 
+    gcprod.company as company_gcprod
+    , gcprod.document_no as document_no_gcprod
+    , zap.company as company_zap
+    , zap.document_no as document_no_zap
+    , numRecords_GCPROD
+    , numRecords_zap
+    , balance_gcprod
+    , balance_zap
+    , IFNULL(balance_zap,0) - IFNULL(balance_gcprod,0) as balance_ZapMinusGCPROD
+    , IFNULL(numRecords_zap,0) - IFNULL(numRecords_GCPROD,0) as numRecords_ZapMinusGCPROD
+FROM 
+    gcprod FULL OUTER JOIN  zap on gcprod.company = zap.company AND gcprod.document_no = zap.document_no
+WHERE 
+    gcprod.document_no IS NULL or zap.document_no IS NULL
+    ABS(IFNULL(balance_zap,0) - IFNULL(balance_gcprod,0)) > 1
+    OR ABS(IFNULL(numRecords_zap,0) - IFNULL(numRecords_GCPROD,0)) > 0
+    
 
 /** investigate a specific company to find variance in records **/
 
@@ -82,3 +128,34 @@ select
 FROM 
     gcprod FULL OUTER JOIN  zap on gcprod.SUMMARYKEY = zap.SUMMARYKEY
 WHERE zap.SUMMARYKEY is null or gcprod.SUMMARYKEY is null
+
+
+/** investigate specific document records **/
+
+SELECT DATEMONTHEND, replace(company,'_','.') as company, "CUSTOMER NO.", "DOCUMENT NO.", "DOCUMENT DATE", "AR MONTHEND BALANCE", "POSTING DATE", "DUE AGED DAYS", "INVOICE AGED DAYS"
+FROM DATAWAREHOUSE.GC_PROD_WH.RAW_ACCOUNTS_RECEIVABLE_MONTHLY
+WHERE company = '20.02-SESC' AND "DOCUMENT NO." = 'PROVDISCCM-30'
+UNION
+SELECT DATEMONTHEND, replace(company,'_','.') as company, "CUSTOMER NO.", "DOCUMENT NO.", "DOCUMENT DATE", "AR MONTHEND BALANCE", "POSTING DATE", "DUE AGED DAYS", "INVOICE AGED DAYS"
+FROM DATAWAREHOUSE.ZAP_BIZ_CENTRAL.STAGE_ACCOUNTS_RECEIVABLE_MONTHLY
+WHERE company = '20.02-SESC' AND "DOCUMENT NO." = 'PROVDISCCM-30'
+ORDER BY source 
+
+/** Testing joins with due date **/
+with tmp as (
+    SELECT *
+    FROM DATAWAREHOUSE.GC_PROD_WH.RAW_ACCOUNTS_RECEIVABLE_MONTHLY
+    WHERE company = '20.02-SESC' AND "DOCUMENT NO." = 'PROVDISCCM-30'
+    UNION
+    SELECT *
+    FROM DATAWAREHOUSE.ZAP_BIZ_CENTRAL.STAGE_ACCOUNTS_RECEIVABLE_MONTHLY
+    WHERE company = '20.02-SESC' AND "DOCUMENT NO." = 'PROVDISCCM-30'
+)
+select 
+    fnl.*
+    , due_age."BUCKET SHORT"
+from tmp fnl 
+LEFT join DATAWAREHOUSE.GC_PROD_WH.RAW_AGING_BUCKET_BY_COMPANY due_age
+    on TRIM(replace(fnl."COMPANY",'_','.')) =  TRIM(replace(due_age.COMPANY,'_','.')  ) 
+    and fnl."DUE AGED DAYS" >= due_age."FROM"
+    and fnl."DUE AGED DAYS" <  due_age."TO"
