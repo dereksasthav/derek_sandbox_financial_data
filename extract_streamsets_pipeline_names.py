7b7e30d05579578e7b8f6b7b5b53b758e89f8029
dereksasthav <84297324+dereksasthav@users.noreply.github.com>
2021-06-28 16:44:00 -0500

    adding code to extract pipeline id

--- extract_streamsets_pipeline_names.py
+++ extract_streamsets_pipeline_names.py
@@ -3,9 +3,19 @@ import pandas as pd
 import os
 
 # Set file path properties
-base_file_path = 'C:\Users\DerekSasthav\OneDrive - Generate Capital\Desktop\streamsets'
+base_file_path = 'C:\\Users\\DerekSasthav\\OneDrive - Generate Capital\\Desktop\\streamsets'
+titles = []
 
 # loop through each file in directory
-for filename in os.lisdir(base_file_path):
+for filename in os.listdir(base_file_path)[:2]:
     if filename.endswith(".json"):
         print(filename)
+        full_filename = base_file_path + '\\' + filename
+        with open(full_filename) as f:
+            data = json.load(f) 
+            title = data["pipelineConfig"]["title"]
+            pipelineID = data["pipelineConfig"]["pipelineId"]
+            payload = {'title':title, 'pipelineID':pipelineID}
+            print(pipelineID, title)
+
+print(titles)
