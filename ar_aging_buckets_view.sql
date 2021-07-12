9ea5bdf8755d2397a0de6e76128245ef5916cfee
dereksasthav <84297324+dereksasthav@users.noreply.github.com>
2021-07-12 12:33:44 -0500

    Create ar_aging_buckets_view.sql

---
+++ ar_aging_buckets_view.sql
@@ -0,0 +1,85 @@
+WITH buckets AS (
+
+    SELECT * FROM DATAWAREHOUSE.GC_PROD_WH.RAW_AGING_BUCKET_BY_COMPANY
+
+), ar_aging AS (
+
+    SELECT * FROM DATAWAREHOUSE.GC_PROD_WH.RAW_ACCOUNTS_RECEIVABLE_MONTHLY
+
+), all_companies AS (
+
+    SELECT DISTINCT COMPANY
+    FROM ar_aging
+
+), buckets_companies AS (
+
+    SELECT DISTINCT COMPANY
+    FROM buckets
+
+), missing_companies AS (
+
+    SELECT COMPANY 
+    FROM all_companies
+    EXCEPT
+    SELECT COMPANY 
+    FROM buckets_companies
+
+), default_buckets AS (
+
+    SELECT *
+    FROM buckets
+    WHERE COMPANY = '10.01-GCI'
+
+
+), missing_buckets AS (
+
+    SELECT 
+        abbc_in."BUCKET ID"
+        , abbc_in."BUCKET TYPE"
+        , mc.COMPANY
+        , abbc_in."BUCKET SHORT"
+        , abbc_in."BUCKET DESC FOR DUE"
+        , abbc_in."BUCKET DESC FOR INVOICE"
+        , abbc_in."FROM"
+        , abbc_in."TO"
+        , abbc_in."DIRECTION FOR DUE"
+        , abbc_in."DIRECTION FOR INVOICE"
+    FROM missing_companies mc 
+    CROSS JOIN default_buckets abbc_in 
+
+
+), unioned_data AS (
+
+    SELECT 
+         "BUCKET ID"
+        , "BUCKET TYPE"
+        , COMPANY
+        , "BUCKET SHORT"
+        , "BUCKET DESC FOR DUE"
+        , "BUCKET DESC FOR INVOICE"
+        , "FROM"
+        , "TO"
+        , "DIRECTION FOR DUE"
+        , "DIRECTION FOR INVOICE"
+    FROM missing_buckets
+    
+    UNION 
+
+    SELECT 
+         "BUCKET ID"
+        , "BUCKET TYPE"
+        , COMPANY
+        , "BUCKET SHORT"
+        , "BUCKET DESC FOR DUE"
+        , "BUCKET DESC FOR INVOICE"
+        , "FROM"
+        , "TO"
+        , "DIRECTION FOR DUE"
+        , "DIRECTION FOR INVOICE"
+    FROM buckets
+
+
+)
+
+SELECT * 
+FROM unioned_data
