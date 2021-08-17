1e82e81473653d5015a54c569fff7bda1af579ec
dereksasthav <84297324+dereksasthav@users.noreply.github.com>
2021-08-17 17:23:19 -0500

    Update adaptive_gl_accounts_alert.sql

--- adaptive_gl_accounts_alert.sql
+++ adaptive_gl_accounts_alert.sql
@@ -1,10 +1,24 @@
 WITH adaptive_src AS (
     
-    SELECT * FROM "DATAWAREHOUSE"."ADAPTIVE_INSIGHTS"."STAGE_LU_ADAPTIVE_INSIGHTS_ACCOUNT"
+    SELECT * FROM "DATAWAREHOUSE"."ADAPTIVE_INSIGHTS"."RAW_LU_ADAPTIVE_INSIGHTS_ACCOUNT"
 
-), bc_src AS (
+), bs_src AS (
 
-    SELECT * FROM 
+    SELECT * FROM "DATAWAREHOUSE"."FPA_TO_ADAPTIVE"."VRAW_NAV_BALANCEREPORTSOURCE"
+
+), is_src AS (
+
+    SELECT * FROM "DATAWAREHOUSE"."FPA_TO_ADAPTIVE"."VRAW_NAV_INCOMEREPORTSOURCE"
+
+), actual_accounts AS (
+
+    SELECT DISTINCT companycode, account, periodenddate
+    FROM bs_src
+
+    UNION
+
+    SELECT DISTINCT companycode, account, periodenddate
+    FROM is_src 
 
 ), adaptive AS (
 
@@ -19,20 +33,38 @@ WITH adaptive_src AS (
         , account_child3_name
         , account_child4_name
         , account_child5_name
+        , account_child6_name 
         , COALESCE(
+            account_child6_name,
             account_child5_name,
             account_child4_name,
             account_child3_name,
             account_child2_name,
             account_child1_name,
             account_name
-        ) AS adaptive_account_name 
+        ) AS adaptive_account_name
+        , TRIM(SPLIT_PART(COALESCE(
+            account_child6_name,
+            account_child5_name,
+            account_child4_name,
+            account_child3_name,
+            account_child2_name,
+            account_child1_name,
+            account_name
+        ),' - ',0)) AS adaptive_account_code 
     FROM adaptive_src
     WHERE 1=1
-        AND sheet like 'Balance Sheet'
         
 )
 
-SELECT *
-FROM adaptive 
-
+SELECT DISTINCT
+    actual_accounts.companycode
+    , actual_accounts.account 
+    , LAST_DAY(actual_accounts.periodenddate,'month') as periodenddate 
+FROM actual_accounts
+LEFT JOIN adaptive 
+    ON actual_accounts.account = adaptive_account_code
+WHERE 1=1
+    AND adaptive.adaptive_account_code IS NULL
+    AND periodenddate = '2021-07-31'
+ORDER BY periodenddate DESC, actual_accounts.companycode, actual_accounts.account 
