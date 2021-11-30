8dd06515f510bc941cbfd80d6e6a68738a9e2b4e
dereksasthav <84297324+dereksasthav@users.noreply.github.com>
2021-11-30 11:05:22 -0800

    Update sample_ru_upload.py

--- sample_ru_upload.py
+++ sample_ru_upload.py
@@ -1,10 +1,32 @@
 import pandas as pd
 import datetime 
-fileName = 'C:\\Users\\DerekSasthav\\OneDrive - Generate Capital\\Desktop\\STEM LCR Model 09.15.2021 FPA Edit (V2)_r7.xlsx'
+
+# Define function for column checking:
+def column_check(x):
+    if 'unnamed' in str(x).lower():
+        return False
+    if str(x).lower() in ['account', 'level', 'split label']:
+        return True 
+    if isinstance(x, datetime.datetime):
+        return True
+    return True
+
+fileName = 'C:\\Users\\DerekSasthav\\OneDrive - Generate Capital\\Desktop\\test_RU.xlsx'
+#fileName = 'test_RU.xlsx'
+
 tabName = 'RU'
 
 start = datetime.datetime.now()
-df = pd.read_excel(fileName, tabName, header=5  )
+df = pd.read_excel(fileName, tabName, header=4,engine = 'openpyxl', usecols = column_check)
 end = datetime.datetime.now()
-print(df.head())
+#print([isinstance(x, datetime.datetime) for x in list(df.columns)])
+#print(df.head(20))
 print(end-start)
+
+
+# remove bad records
+firstNullIndex = min(df["Account"].loc[df["Account"].isnull()].index.to_list())
+dfClean = df.loc[:firstNullIndex-1,]
+print(dfClean.head(10))
+
+
